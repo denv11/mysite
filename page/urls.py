1118849cb4a7577a362d021e8afd988bc6e30d64
denv11 <denis_bogd@mail.ru>
2016-10-31 17:08:12 +0500

    31-10-2016 по книге Практика создания

--- page/urls.py
+++ page/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import  url
 from page import views
 urlpatterns = [
-    (
-                       url(r'^(?:(?P<id>\d+))?$', views.index, name = 'index'),
-                       url(r'^good/(?P<id>\d+))$', views.good, name = 'good'),
-                       )
-]
+
+                       url(r'^(?P<cat_id>[0-9])+/$', views.index, name ='index'),
+                       url(r'^good/(?P<good_id>\d+)/$', views.good, name = 'good'),
+                       url(r'^categorys/$', views.categorys, name= 'categorys'),
+                       ]
